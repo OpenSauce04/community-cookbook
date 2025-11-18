acd86f7ff9878cf13189e35f81b0a5af90de9206
OpenSauce04 <opensauce04@gmail.com>
2025-11-18 16:51:33 +0000

    Added title to all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Link } from 'react-router';
 
 import { HomePage } from './pages/HomePage.jsx';
 import { SearchPage } from './pages/SearchPage.jsx';
@@ -6,6 +6,9 @@ import { SearchPage } from './pages/SearchPage.jsx';
 export default function App() {
   return (
     <BrowserRouter>
+      <h1>
+        <Link to='/'>Community Cookbook</Link>
+      </h1>
       <Routes>
         <Route path='/' element={ <HomePage /> } />
         <Route path='/search' element={ <SearchPage /> } />
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import { SearchBar } from '../components/SearchBar.jsx'
 export function HomePage() {
   return (
       <>
-        <h2>Community Cookbook</h2>
         <SearchBar />
       </>
   )
